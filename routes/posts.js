cb498e08abaa5a8b9b7f6617eec2699bc2c8b2eb
Safia <safiaali@hotmail.com>
2021-04-29 14:36:11 +0100

    create posts.js structure, create htmlPostForm + displayPosts, import layout.js, model.js

--- routes/posts.js
+++ routes/posts.js
@@ -1,9 +1,36 @@
-function get(request, response){
+const layout = require('../layout');
+const model = require('../database/model.js');
+
+function htmlPostForm() {
+    return `
+
+    // insert from html here
+
+    `;
+}
+
+function displayPosts() {
+    return `
+
+    // insert html here for posts layout ect divs with posts and delete button 
+
+    `;
+}
+
+
+function get(request, response) {
+
+    const html = layout(
+      `Checkin'?`,
+        htmlPostForm() + displayPosts()
+    );
+    response.send(html);
 
 }
 
-function post(request, response){
-    
+function post(request, response) {
+
+
 }
 
-module.exports = {get,post}
+module.exports = { get, post };
