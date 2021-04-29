f5a885a7cd0cd67a374c2feb0f08cee294f3ceb7
Safia <safiaali@hotmail.com>
2021-04-29 15:25:29 +0100

    create posts.post route and model.createPost and model.getUserSessionData Relates #3
    Co-authored-by: designkay <designkay@gmail.com>

--- routes/posts.js
+++ routes/posts.js
@@ -2,35 +2,44 @@ const layout = require('../layout');
 const model = require('../database/model.js');
 
 function htmlPostForm() {
-    return `
-
-    // insert from html here
-
+  return `
+    <h1>Checkin'?</h1>
+    <form action="/posts" method="POST">
+        <p>
+            <label for="text_content"> Leave your question here! </label>
+            <textarea name= "text_content" id="text_content" required>
+            </textarea>
+        </p>
+        <button type="submit">Submit</button>
+    </form>
+    <a href="/">Back to Homepage</a>
     `;
 }
 
 function displayPosts() {
-    return `
+  return `
 
-    // insert html here for posts layout ect divs with posts and delete button 
+    // insert html here for posts layout ect divs with posts and delete button
 
     `;
 }
 
-
 function get(request, response) {
-
-    const html = layout(
-      `Checkin'?`,
-        htmlPostForm() + displayPosts()
-    );
-    response.send(html);
-
+  const html = layout(`Checkin'?`, htmlPostForm() + displayPosts());
+  response.send(html);
 }
 
 function post(request, response) {
-
-
+  const { text_content } = request.body;
+    const sid = request.signedCookies.sid;
+    if (sid) {
+        model.getUserSessionData(sid)
+            .then((result) => {
+                const user_id = result.data.user.id;
+                return model.createPost(user_id, text_content);
+            })
+            .then(response.redirect('/posts'));
+    }
 }
 
 module.exports = { get, post };
