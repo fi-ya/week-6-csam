a0d7d23cc0f171d5c9e142fd3b9521f9349afa51
maryamgh1988 <ghorbani.maryam6688@gmail.com>
2021-04-30 12:09:45 +0100

    adding meta description and some css

--- layout.js
+++ layout.js
@@ -5,6 +5,7 @@ function layout(title, content) {
     <head>
       <meta charset="UTF-8">
       <meta name="viewport" content="width=device-width, initial-scale=1">
+      <meta name="description" content="Check in question examples">
       <link rel='stylesheet' href='styles.css'>
       <link rel="preconnect" href="https://fonts.gstatic.com">
       <link href="https://fonts.googleapis.com/css2?family=Reem+Kufi&display=swap" rel="stylesheet">
--- routes/home.js
+++ routes/home.js
@@ -2,7 +2,7 @@ const layout = require('../layout')
 
 function get(request, response) {
   const html = `
-  <h1><img class='logo' src='logo.png'></h1>
+  <h1><img class='logo' src='logo.png' alt="checkin logo"></h1>
     <a href="/sign-up">Sign up</a>
     <br>
     <a href="/log-in">Log in</a>
